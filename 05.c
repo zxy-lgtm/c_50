9e214ee5a4833395830e307129246f6e94834862
zxy-lgtm <1563925535@qq.com>
2020-10-16 01:13:58 +0800

    05.

--- 05.c
+++ 05.c
@@ -2,23 +2,31 @@
 int sp(int n)
 {
     int count = 0;
-    int c;
     for (int j=2;j<n;j++){
         if (n%j==0)
-            count +=1;
-    }
+            count ++;
+    } 
     if (count==0)
-        c = n;
-    return c;
+    return n; 
+    else
+    {
+        return 0;
+    }
+    
+
 }
 int main(void)
 {
     int n=100;
-    for (int i = 0;i < 100;i++){
-        printf("%d ",sp(n));
-        n--;
-        if (n%5==0)
+    int count=0;
+    for (int i = 0;i < 99;i++){
+        if (sp(n)!=0){
+            printf("%d ",sp(n));
+            count ++;
+        }
+        if (count%5==0)
         printf("\n");
+        n --;
     }
     return 0;
 }
