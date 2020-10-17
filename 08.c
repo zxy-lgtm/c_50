96051811d477d824054bdc5930f959d1429eae4f
zxy-lgtm <1563925535@qq.com>
2020-10-17 13:47:15 +0800

    08.problem

---
+++ 08.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void)
+{
+    int i,j,n;
+    int aver;
+    while(scanf("%d",&n)!=EOF)
+    {
+        int sum = 0;
+        for (i=0;i<n;i++)
+        {
+            scanf("%d",&j);
+            sum+=j;
+        }
+       aver = sum/n ;
+       printf("%.2f\n",aver);
+    }
+    return 0;
+}
