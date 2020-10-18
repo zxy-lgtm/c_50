30cfbc95a911f8ee27d0cd184f9a5063f5d85445
zxy-lgtm <1563925535@qq.com>
2020-10-18 23:41:38 +0800

    03

--- c_03.c
+++ c_03.c
@@ -3,6 +3,7 @@ int char_a(int i)
 {
 	for(int a=0;a<i;a++)
 	putchar('*');
+	printf("\n");
 }
 int char_b(int i)
 {
@@ -12,14 +13,9 @@ int char_b(int i)
 int main(void)
 {
 	char_a(5);
-	printf("\n");
 	char_b(1),char_a(3);
-	printf("\n");
 	char_b(2),char_a(1);
-	printf("\n");
 	char_b(1),char_a(3);
-	printf("\n");
 	char_a(5);
-	printf("\n");
 	return 0;
 }
