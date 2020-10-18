1637d24c476a98e75df1253a06ab9c1df2453b41
zxy-lgtm <1563925535@qq.com>
2020-10-18 23:39:34 +0800

    03

--- c_03.c
+++ c_03.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
+int char_a(int i)
+{
+	for(int a=0;a<i;a++)
+	putchar('*');
+}
+int char_b(int i)
+{
+	for (int a=0;a<i;a++)
+	putchar(' ');
+}
 int main(void)
 {
-	printf("*****\n");
-	printf(" ***\n");
-	printf("  *\n");
-	printf(" ***\n");
-	printf("*****\n");
+	char_a(5);
+	printf("\n");
+	char_b(1),char_a(3);
+	printf("\n");
+	char_b(2),char_a(1);
+	printf("\n");
+	char_b(1),char_a(3);
+	printf("\n");
+	char_a(5);
+	printf("\n");
 	return 0;
 }
