376ec0bd553bf7c2fecf67bd78ee43f5ef4e707f
zxy-lgtm <1563925535@qq.com>
2020-10-19 19:46:59 +0800

    13

--- 13.c
+++ 13.c
@@ -1,27 +1,22 @@
 #include<stdio.h>
 int main(void)
 {
-    int n;
-    int count=0;
+    int T;
+    int i;
     do{
-        printf("请输入一个数:");
-        scanf("%d",&n);
-        if(n>1000)
-        printf("请输入一个小于1000的数");
+        scanf("%d",&T);
+        if(T>10)
+        printf("请输入一个小于等于10的数\n");
+    }while(T>10);
+    for (i=0;i<T;i++){
+       long int A,B,C;
+        printf("请以number number number的形式输出");
+        scanf("%d %d %d",&A,&B,&C);
+        int D=A+B;
+        if(D>C)
+        printf("Case #x: ture\n");
         else
-        printf("%d\n",n);
-    }while(n>1000);
-    
-    do{
-        if(n%2==0){
-            n=n/2;
-            count++;
-        }
-        else{
-            n=(3*n+1)/2;
-            count++;
-        }
-    }while(n!=1);
-    printf("%d\n",count);
+        printf("Case #x: false\n");
+    }
     return 0;
 }
