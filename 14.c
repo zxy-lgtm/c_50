011afe24607413323b68343f661604b4b9f7c5dc
zxy-lgtm <1563925535@qq.com>
2020-10-21 00:47:06 +0800

    14

--- 14.c
+++ 14.c
@@ -1,29 +1,28 @@
 #include<stdio.h>
-int swap(int *pa,int *pb)
-{
-    int temp=*pa;
-    *pa=*pb;
-    *pb=temp;
-    return 0;
-}
 int main(void)
 {
-    int n,min;
-    int count=0;
+    int n,min,count;
     int a[100]={0};
-    int i;
+    int i,j;
     while(scanf("%d",&n) != EOF)
     {
+        if(n==0)
+        break;
+        else
         for (i=0;i<n;i++){
             scanf("%d",&a[i]);
+            min=a[0];
+            count = 0;
         }
         for(i=0;i<n;i++)
         {
-            min=a[0];
-            if(min>a[i])
-                count++;
-        }
-        swap(&a[count],&a[0]);
+            if(min>a[i]){
+                min=a[i];
+                count=i;
+            }
+        }     
+        a[count]=a[0];
+        a[0]=min;
         for (i=0;i<n;i++){
             printf("%d",a[i]);
     }
