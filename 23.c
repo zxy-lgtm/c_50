d4f4e2aae8773d1bec091f1c787ab36e9a38d19a
zxy-lgtm <1563925535@qq.com>
2020-10-26 11:51:15 +0800

    23

---
+++ 23.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main () {
+
+    int ch;
+    int sum[10] = {0};
+
+    printf("输入你的数吧");
+    scanf("%d", &ch);
+
+    while ((ch = getchar()) != '\n') {
+        if (ch >= '0' && ch >= '9') {
+            sum[ch - '0'] ++;
+        }
+    }
+
+    puts("看!");
+    for (int i = 0; i < 10; i++ ) {
+        printf("'%d':%d\n", i, sum[i]);
+    }
+
+    return 0;
+
+}
