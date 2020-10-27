0e9733dc9e1a427a85dc1c10a2835a8c64aef7fb
zxy-lgtm <1563925535@qq.com>
2020-10-27 20:52:22 +0800

    07

--- 07.c
+++ 07.c
@@ -1,20 +1,25 @@
-#include<stdio.h>
-int main(void)
-{
-    int i,j;
+#include <stdio.h>
+
+int main () {
+
+    int i, j;
     int n;
-    while(scanf("%d",&n)!=EOF)
-    {
-        int sum=1;
-        for (i=0;i<n;i++)
-        {
-            scanf("%d",&j);
-            if(j%2==0)
-                sum*=1;
-            else 
-                sum*=j;
+
+    while (scanf("%d", &n) != EOF) {
+
+        int sum = 1;
+        
+        for (i = 0; i < n; i++) {
+            scanf("%d", &j);
+
+            if (j % 2 == 0)
+                sum *= 1;
+            else
+                sum *= j;
         }
-        printf("%d",sum);
+
+        printf("%d\n", sum);
     }
+    
     return 0;
 }
