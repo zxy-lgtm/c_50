3ab1cca81d184f143e59b9e0564b877c052badcb
zxy-lgtm <1563925535@qq.com>
2020-11-13 15:03:15 +0800

    4

--- go/4.go
+++ go/4.go
@@ -5,13 +5,15 @@ import (
 )
 
 func main() {
-	var number1, number2, divisor int
+	var number1, number2 int
 	fmt.Scanf("%d %d", &number1, &number2)
-	divisor = common_divisor(number1, number2)
-	fmt.Println(divisor)
+	divisor1 := common_divisor1(number1, number2)
+	fmt.Println(divisor1)
+	divisor2 := common_divisor2(number1, number2)
+	fmt.Println(divisor2)
 }
 
-func common_divisor(n1 int, n2 int) int {
+func common_divisor1(n1 int, n2 int) int {
 
 	for n1 != n2 {
 		if n1 > n2 {
@@ -21,5 +23,15 @@ func common_divisor(n1 int, n2 int) int {
 		}
 	}
 	return n1
+}
 
+func common_divisor2(n1 int, n2 int) int {
+
+	var divisor int
+	if n1%n2 == 0 {
+		divisor = n2
+	} else {
+		divisor = common_divisor2(n2, n1%n2)
+	}
+	return divisor
 }
