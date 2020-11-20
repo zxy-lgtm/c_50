be2da13c4240817db832121b01d7d02dc0e77d29
zxy-lgtm <1563925535@qq.com>
2020-11-20 17:10:29 +0800

    two way

--- go/ld_1.go
+++ go/ld_1.go
@@ -14,9 +14,24 @@ func twoSum(nums []int, target int) []int {
 	return nil
 }
 
+//利用map来解题
+func twoSum2(nums []int, target int) []int {
+	slice := []int{}
+	map1 := make(map[int]int)
+	for i, k := range nums {
+		if value, exist := map1[target-k]; exist {
+			slice = append(slice, value)
+			slice = append(slice, i)
+		}
+		map1[k] = i
+	}
+	return slice
+}
+
 func main() {
 	fmt.Println("两数之和:\n下标为:")
 	nums := []int{2, 7, 11, 15}
 	target := 9
 	fmt.Println(twoSum(nums, target))
+	fmt.Println(twoSum2(nums, target))
 }
