800aefe1fb99534b03d98fcd7c0307385371c274
zxy-lgtm <1563925535@qq.com>
2020-11-23 19:13:36 +0800

    2

--- exam/newtask2.go
+++ exam/newtask2.go
@@ -20,15 +20,14 @@ func romanToInt(s string) int {
 		"CM": 900,
 	}
 
-	//idx是工具人,sum是返回值
-	idx, sum := 0, 0
+	//sum是返回值
+	sum := 0
 	//将字符串转化为byte型数组切片,方便遍历
 	romanBytes := []byte(s)
-	//算出长度,结束的条件
-	alen := len(romanBytes)
-	for idx < alen {
+
+	for idx := 0; idx < len(romanBytes); idx++ {
 		//这是为了算像III这种情况的罗马数字,因为II没有在map里面,它将不会执行下面那个if语句里的内容...
-		if idx < alen-1 {
+		if idx < len(romanBytes)-1 {
 			/*因为map中为string类型,所以要进行一次转换,而因为map中的key最多两个byte,我们不妨直接一次性转化两个byte
 			,下面这个判断是ok存在就执行*/
 			if v, ok := romanMap[string(romanBytes[idx:idx+2])]; ok {
@@ -38,14 +37,13 @@ func romanToInt(s string) int {
 			}
 		}
 		sum += romanMap[string(romanBytes[idx])]
-		idx++
 
 	}
 	return sum
 }
 
 func main() {
-	number := "IIIVX"
+	number := "XI"
 	newnumber := romanToInt(number)
 	fmt.Println(newnumber)
 }
